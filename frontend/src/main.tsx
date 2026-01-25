ed438823636b5e7b59d45c61f5cab1f0d432925d
magguagnelli <mag.guagnelli@gmail.com>
2026-01-25 16:39:22 -0600

    Add Login app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 //import './styles/estilos_publicos.css'
-//import './styles/estilos_generales.css'
+//import './styles/estilos_general.css'
 //import './styles/free.gob.mx.2024.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(

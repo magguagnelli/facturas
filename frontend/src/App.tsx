f65c0e18a926d9a29c0716df69701ea584722a4e
magguagnelli <mag.guagnelli@gmail.com>
2026-01-25 14:50:14 -0600

    Login front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,11 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-
-interface ApiResponse {
-  message: string
-}
+import Login from "./components/Login";
 
 function App() {
-  const [apiLogin, setApiData] = useState<ApiResponse | null>(null)
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    // Fetch both hello message and chart data
-    Promise.all([
-      fetch('/api/login').then(response => response.json()),
-    ])
-      .then(([loginData]) => {
-        setApiData(loginData)
-        setLoading(false)
-      })
-      .catch(error => {
-        console.error('Error:', error)
-        setLoading(false)
-      })
-  }, [])
-   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Sistema de Facturas</h1>
-        <div className="content">
-          {
-            loading
-              ? <p>Cargando datos de la API...</p>
-              : apiLogin
-                  ? <p>{apiLogin.title}</p>
-                  : <p>Error al cargar los datos de la API.</p>
-          }
-        </div>
-      </header>
+  return (
+    <div>
+      <Login />
     </div>
-  )
+  );
 }
 
-export default App 
+export default App;

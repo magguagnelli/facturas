f767501a42d4367b7d60bf672c395942f5f584eb
magguagnelli <mag.guagnelli@gmail.com>
2026-01-25 16:22:56 -0600

    Home page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import './css/estilos_publicos.css'
+import './css/estilos_generales.css'
+import './css/free.gob.mx.2024.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

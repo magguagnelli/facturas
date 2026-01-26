d0994eafde7097806330401197bdd956cc4349b9
magguagnelli <mag.guagnelli@gmail.com>
2026-01-25 19:24:36 -0600

    Form Login

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,53 +1,74 @@
 import React from "react";
 import './Login.css';
 
-const Login: React.FC = () => {
-  return 
-    <div class="boxGral">   
-        <div class="boxLogin" style="display:block">
-            <div class="headerBox">
-                <img src="/img/PLECA_LOGO.png" />
-            </div>
-            <div class="bodyBox">
-                <form id="form_log" method="POST" action="registro" class="row" data-gtm-form-interact-id="0">
-                    <div class="col-12 ma-g">
-                        <label class="titForm verde_t">
-                            <img src="/img/USUARIO.svg" >USUARIO
-                        </label>
-                        <input type="text" id="user" name="user" class="form-control form-login oro_t">
-                    </div>
-                    <div class="col-12 ma-g">
-                        <label class="titForm verde_t"><img src="/img/CONTRASENA.svg">CONTRASEÑA</label>
-                        <input type="password" id="pass" name="pass" class="form-control form-login oro_t" data-gtm-form-interact-field-id="0">
-                        <a href="#" onclick="mostrarRestContrasena()" class="pass-olvide">Olvidé mi contraseña</a>
-                    </div>
-                    
-                    <div id="recaptcha_log" class="col-12 ma-g g-recaptcha" data-sitekey="6Lf15TgpAAAAAEDQQnfHOgr8kvxEAxRN4ri4VKAv">
-                        <div style="width: 304px; height: 78px;"><div>
-                        <iframe title="reCAPTCHA" width="304" height="78" role="presentation" 
-                            name="a-3s3sztd6ke8i" 
-                            frameborder="0" 
-                            scrolling="no" 
-                            sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation allow-modals allow-popups-to-escape-sandbox allow-storage-access-by-user-activation" 
-                            src="https://www.google.com/recaptcha/api2/anchor?ar=1&amp;k=6Lf15TgpAAAAAEDQQnfHOgr8kvxEAxRN4ri4VKAv&amp;co=aHR0cHM6Ly9hcHAtcmVnaXN0cm8uaW1zc2JpZW5lc3Rhci5nb2IubXg6NDQz&amp;hl=es-419&amp;v=N67nZn4AqZkNcbeMu4prBgzg&amp;size=normal&amp;anchor-ms=20000&amp;execute-ms=30000&amp;cb=pd1mfxjz90f0">
-                        </iframe>
-                    </div>
-                    <textarea id="g-recaptcha-response-1" name="g-recaptcha-response" class="g-recaptcha-response" 
-                        style="width: 250px; height: 40px; border: 1px solid rgb(193, 193, 193); margin: 10px 25px; padding: 0px; resize: none; display: none;">
-                    </textarea>
-                    <div class="col-12 t-c">
-                        <button id="loginBtn" class="btnGral verde_t" type="submit">
-                            <p>INGRESAR</p>
-                            <div class="btnIcon">
-                                <div class="boxIcon verde"><img src="assets/img/CONTINUAR.svg"></div>
-                                <span class="s-verde"></span>
-                            </div>
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-};
+export default function Login() {
+  const mostrarRestContrasena = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    // TODO: aquí abres modal o navegas a /reset
+    console.log("Olvidé mi contraseña");
+  };
 
-export default Login;
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // TODO: login
+    console.log("submit login");
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-6">
+          <form onSubmit={onSubmit}>
+            <div className="col-12 ma-g">
+              <label className="titForm verde_t">
+                <img src="/img/USUARIO.svg" alt="Usuario" /> USUARIO
+              </label>
+
+              <input
+                type="text"
+                id="user"
+                name="user"
+                className="form-control form-login oro_t"
+              />
+            </div>
+
+            <div className="col-12 ma-g">
+              <label className="titForm verde_t">
+                <img src="/img/CONTRASENA.svg" alt="Contraseña" /> CONTRASEÑA
+              </label>
+
+              <input
+                type="password"
+                id="pass"
+                name="pass"
+                className="form-control form-login oro_t"
+              />
+
+              <a href="#" onClick={mostrarRestContrasena} className="pass-olvide">
+                Olvidé mi contraseña
+              </a>
+            </div>
+
+            {/* Si vas a usar reCAPTCHA real, normalmente se integra con script o librería */}
+            <div id="recaptcha_log" className="col-12 ma-g">
+              {/* placeholder */}
+            </div>
+
+            <div className="col-12 ma-g">
+              <button type="submit" className="btnLogin">
+                <p>INGRESAR</p>
+
+                <div className="btnIcon">
+                  <div className="boxIcon verde">
+                    <img src="/img/CONTINUAR.svg" alt="Continuar" />
+                  </div>
+                  <span className="s-verde" />
+                </div>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}

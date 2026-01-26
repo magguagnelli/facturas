2838f97b0f5c22345915d98e48e059434343ee4f
magguagnelli <mag.guagnelli@gmail.com>
2026-01-26 14:34:20 -0600

    Facturas form

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,54 +1,56 @@
-import React from "react";
-import './Login.css';
+import React, { useState } from "react";
 
 export default function Login() {
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+
   const mostrarRestContrasena = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
-    // TODO: aquí abres modal o navegas a /reset
+    // TODO: aquí navega a /reset o abre un modal
     console.log("Olvidé mi contraseña");
   };
 
   const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // TODO: login
-    console.log("submit login");
+    // TODO: aquí tu lógica real de login
+    console.log({ user, pass });
   };
 
   return (
-    <div class="titSecciones center">
+    <div className="titSecciones center">
         <h3>INICIO DE SESIÓN</h3>
     </div>
-    <div class="boxLogin">
-        <div class="headerBox">
+    <div className="boxLogin">
+        <div className="headerBox">
             <img src="/img/PLECA_LOGO.png" />
         </div>
-        <div class="bodyBox">
-            <form id="form_log" method="POST" action="registro" class="row" data-gtm-form-interact-id="0">
-                <div class="col-12 ma-g">
-                    <label class="titForm verde_t">
+        <div className="bodyBox">
+            <form id="form_log" method="POST" action="registro" className="row" data-gtm-form-interact-id="0">
+                <div className="col-12 ma-g">
+                    <label className="titForm verde_t">
                         <img src="/img/USUARIO.svg" />USUARIO
                     </label>
-                    <input type="text" id="user" name="user" class="form-control form-login oro_t" />
+                    <input type="text" id="user" name="user" className="form-control form-login oro_t" />
                 </div>
-                <div class="col-12 ma-g">
-                    <label class="titForm verde_t">
+                <div className="col-12 ma-g">
+                    <label className="titForm verde_t">
                         <img src="/img/CONTRASENA.svg" />CONTRASEÑA
                     </label>
-                    <input type="password" id="pass" name="pass" class="form-control form-login oro_t" data-gtm-form-interact-field-id="0" />
-                    <a href="#" onclick="mostrarRestContrasena()" class="pass-olvide">Olvidé mi contraseña</a>
+                    <input type="password" id="pass" name="pass" className="form-control form-login oro_t" data-gtm-form-interact-field-id="0" />
+                    <a href="#" onclick="mostrarRestContrasena()" className="pass-olvide">Olvidé mi contraseña</a>
                 </div>
                 {/* Si vas a usar reCAPTCHA real, normalmente se integra con script o librería */}
                 <div id="recaptcha_log" className="col-12 ma-g">
                 {/* placeholder */}
                 </div>                                          
-                <div class="col-12 t-c">
-                    <button id="loginBtn" class="btnGral verde_t" type="submit">
+                <div className="col-12 t-c">
+                    <button id="loginBtn" className="btnGral verde_t" type="submit">
                         <p>INGRESAR</p>
-                        <div class="btnIcon">
-                            <div class="boxIcon verde">
+                        <div className="btnIcon">
+                            <div className="boxIcon verde">
                                 <img src="/img/CONTINUAR.svg" />
                             </div>
-                            <span class="s-verde"></span>
+                            <span className="s-verde"></span>
                         </div>
                     </button>
                 </div>

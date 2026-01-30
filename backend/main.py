69df5f52ae1cb2dc56fee033e07fc7b142f5899b
magguagnelli <mag.guagnelli@gmail.com>
2026-01-29 20:33:00 -0600

    Testing database conn

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,9 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from databricks import sql
 
-conn = sql.connect(
-    server_hostname="adb-7405615568530351.11.azuredatabricks.net",
-    http_path="/sql/1.0/warehouses/605b873f22449da5",
-    access_token="dapi927b2dd86f775c5296eb785afff3053a",
-)
+    DATABRICKS_HOST ="instance-ee9c166c-7472-4aa4-b57c-7234a9730321.database.azuredatabricks.net"
+    HTTP_PATH ="/sql/1.0/warehouses/605b873f22449da5"
+    TOKEN = "dapi927b2dd86f775c5296eb785afff3053a"
 
 # --- Logging Setup ---
 app = FastAPI(title="Sistema de Facturas")
@@ -18,4 +16,16 @@ app = FastAPI(title="Sistema de Facturas")
 #loggs
 @app.get("/api/login")
 def login():
+    with sql.connect(
+        server_hostname=DATABRICKS_HOST,
+        http_path=HTTP_PATH,
+        access_token=TOKEN,
+    ) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT current_user(), current_catalog(), current_schema()")
+            print(cur.fetchall())
+
+            cur.execute("SELECT * FROM catalogo.esquema.mi_tabla LIMIT 10")
+            rows = cur.fetchall()
+            print(rows)
     return {"message":"Login con conexión a BD"}
